db6b73a187776ecd1f796ed9655ba847e708a92c
Vardan Margaryan <v.t.margaryan@gmail.com>
2017-11-30 19:46:57 +0300

    FIXED T09.03

--- Lesson09-ToDo-List/T09.03-Exercise-UriMatcher/app/src/main/java/com/example/android/todolist/data/TaskContentProvider.java
+++ Lesson09-ToDo-List/T09.03-Exercise-UriMatcher/app/src/main/java/com/example/android/todolist/data/TaskContentProvider.java
@@ -30,18 +30,18 @@ public class TaskContentProvider extends ContentProvider {
     // DONE (1) Define final integer constants for the directory of tasks and a single item.
     // It's convention to use 100, 200, 300, etc for directories,
     // and related ints (101, 102, ..) for items in that directory.
-    private static final int TASKS = 100;
-    private static final int TASKS_WITH_ID = 101;
+    public static final int TASKS = 100;
+    public static final int TASK_WITH_ID = 101;
 
     // DONE (3) Declare a static variable for the Uri matcher that you construct
     private static UriMatcher sUriMatcher = buildUriMatcher();
 
     // DONE (2) Define a static buildUriMatcher method that associates URI's with their int match
-    private static UriMatcher buildUriMatcher() {
+    public static UriMatcher buildUriMatcher() {
         UriMatcher uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
 
         uriMatcher.addURI(TaskContract.AUTHORITY, TaskContract.PATH_TASKS, TASKS);
-        uriMatcher.addURI(TaskContract.AUTHORITY, TaskContract.PATH_TASKS + "/#", TASKS_WITH_ID);
+        uriMatcher.addURI(TaskContract.AUTHORITY, TaskContract.PATH_TASKS + "/#", TASK_WITH_ID);
 
         return uriMatcher;
     }
