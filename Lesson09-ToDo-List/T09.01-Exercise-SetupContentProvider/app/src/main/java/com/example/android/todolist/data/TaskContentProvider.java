e8e8e2bb4dddcdd8decd9e6b4fa691cc160eb33f
vardan <v.t.margaryan@gmail.com>
2017-11-30 16:58:48 +0300

    Completed T09.01

--- Lesson09-ToDo-List/T09.01-Exercise-SetupContentProvider/app/src/main/java/com/example/android/todolist/data/TaskContentProvider.java
+++ Lesson09-ToDo-List/T09.01-Exercise-SetupContentProvider/app/src/main/java/com/example/android/todolist/data/TaskContentProvider.java
@@ -22,9 +22,10 @@ import android.database.Cursor;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 
-// TODO (1) Verify that TaskContentProvider extends from ContentProvider and implements required methods
+// DONE (1) Verify that TaskContentProvider extends from ContentProvider and implements required methods
 public class TaskContentProvider extends ContentProvider {
 
+    private TaskDbHelper mDbHelper;
 
     /* onCreate() is where you should initialize anything you’ll need to setup
     your underlying data source.
@@ -33,10 +34,10 @@ public class TaskContentProvider extends ContentProvider {
      */
     @Override
     public boolean onCreate() {
-        // TODO (2) Complete onCreate() and initialize a TaskDbhelper on startup
+        // DONE (2) Complete onCreate() and initialize a TaskDbhelper on startup
         // [Hint] Declare the DbHelper as a global variable
-
-        return false;
+        mDbHelper = new TaskDbHelper(getContext());
+        return true;
     }
 
 
